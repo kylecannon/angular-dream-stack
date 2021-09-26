e4a50766baa998e64b262ca2e034dd56cccacb39
Kyle Cannon <kyle.d.cannon@gmail.com>
2021-09-26 10:12:05 -0700

    chore: update dependencies

--- apps/mfe-toh/src/app/mfe-app.module.ts
+++ apps/mfe-toh/src/app/mfe-app.module.ts
@@ -8,6 +8,5 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class MfeAppModule implements LoadableApp {
   EntryComponent = this.tohApp.EntryComponent;
-  constructor(private tohApp: TohAppModule) {
-  }
+  constructor(private tohApp: TohAppModule) {}
 }
--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/mfe-app',
     '<rootDir>/apps/mfe-counter',
     '<rootDir>/apps/mfe-scoreboard',
-    '<rootDir>/libs/counter',
-    '<rootDir>/libs/scoreboard',
-    '<rootDir>/libs/app-utils',
     '<rootDir>/apps/mfe-toh',
-    '<rootDir>/libs/toh',
-    '<rootDir>/libs/dashboard',
     '<rootDir>/apps/mfe-dashboard',
-    '<rootDir>/libs/feature-app-view',
   ],
 };
